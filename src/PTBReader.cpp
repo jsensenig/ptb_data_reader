91eb000e39e7df12b009583328e34c7150443650
kaikai581 <kaikai5816787@gmail.com>
2015-07-29 13:58:26 -0500

    JCF: bugfix - checksum variable was not being initialized and thus its resulting value was non-reproducible in the Penn boardreader; it is now initialized to 0

--- src/PTBReader.cpp
+++ src/PTBReader.cpp
@@ -416,10 +416,11 @@ void PTBReader::ClientTransmiter() {
     //
     // -- Followed the recipe in
     //    https://en.wikipedia.org/wiki/BSD_checksum
-    uint16_t checksum;
+
+    uint16_t checksum = 0;
     uint8_t*ch_buff = reinterpret_cast<uint8_t*>(eth_buffer);
     for (uint32_t i = 0; i < ipck*4; ++i) {
-      // Recast as uint8_t
+
       checksum = (checksum >> 1) + ((checksum & 0x1) << 15) ;
       checksum += ch_buff[i];
       checksum &= 0xFFFF;
