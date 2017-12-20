b92a6d8382ff5dd69eb89c005d642b5949e3e810
Nuno Barros <nfbarros@hep.upenn.edu>
2017-12-20 12:59:28 -0500

    Refactored the standalone application

--- test/standalone_dump.cpp
+++ test/standalone_dump.cpp
@@ -5,14 +5,13 @@
  *      Author: nbarros
  */
 
-#include "Logger.h"
-//#include "ConfigServer.h"
-//#include "PTBManager.h"
 #include <bitset>
 #include <thread>         // std::this_thread::sleep_for
 #include <chrono>         // std::chrono::seconds
 #include <string>
 #include <fstream>
+#include <iostream>
+
 #include "PracticalSocket.h"
 //extern "C" {
 //#include <pthread.h>         // For POSIX threads
@@ -21,32 +20,43 @@
 //using namespace std;
 
 #include <csignal>
-#include "../src/boardreader.h"
-#include "../src/boardmanager.h"
-#include "../src/boardserver.h"
+#include "boardreader.h"
+#include "boardmanager.h"
+#include "boardserver.h"
 
 using std::cout;
 using std::cerr;
+using std::endl;
 
 using ptb::board_reader;
 using ptb::board_manager;
 using ptb::board_server;
 
 bool g_stop_requested;
+bool g_is_running;
 bool g_is_configured;
-FILE*outfile;
+//FILE*outfile;
 
 void signalHandler( int signum )
 {
     cout << "Interrupt signal (" << signum << ") received.\n";
 
+    // If it is running (taking a run), stop the run
+    if (g_is_running) {
+      // -- Stop the run
+      stop_run();
+      return;
+    }
+    // otherwise stop the application completely
+    g_stop_requested = true;
+
     // cleanup and close up stuff here
     // terminate program
 
     //exit(signum);
-    g_stop_requested = true;
-    std::this_thread::sleep_for(std::chrono::seconds(5));
-    ::exit(0);
+    //g_stop_requested = true;
+    //std::this_thread::sleep_for(std::chrono::seconds(5));
+    //::exit(0);
 }
 
 int send_reset(TCPSocket *sock) {
@@ -55,8 +65,10 @@ int send_reset(TCPSocket *sock) {
   char answer[1000];
   sock->send(start_run,27);
   sock->recv(answer,1000);
-  Log(info,"Got answer [%s]",answer);
+  cout << "Got answer [" << answer << "]" << endl;
+
   g_is_configured = false;
+  g_is_running = false;
   return 0;
 }
 
@@ -66,7 +78,8 @@ int start_run(TCPSocket *sock) {
   char answer[1000];
   sock->send(start_run,27);
   sock->recv(answer,1000);
-  Log(info,"Got answer [%s]",answer);
+  cout << "Got answer [" << answer << "]" << endl;
+  g_is_running = true;
 
   return 0;
 }
@@ -77,30 +90,30 @@ int stop_run(TCPSocket *sock) {
   char answer[1000];
   sock->send(start_run,27);
   sock->recv(answer,1000);
-  Log(info,"Got answer [%s]",answer);
-
+  cout << "Got answer [" << answer << "]" << endl;
+  g_is_running = false;
   return 0;
 }
 
 void send_config(TCPSocket *socksrv) {
   if (!g_is_configured) send_reset(socksrv);
 
-  std::ifstream config("./config/config_default.xml");
-  // Read the whole file contents
-  std::string config_contents((std::istreambuf_iterator<char>(config)), std::istreambuf_iterator<char>());
-  //std::string config_contents;
-  //config >> config_contents;
-  config.close();
+  // -- For now the configuration is still minimal
+  // also because the structure has not been decided yet.
+  // it is also implemnted using XML yet...eventually might want to do it in
+  // json or some other fancy way
+  // The only parameter that is being used at the moment is really the destination of the data
+  std::string config_contents = "<config><DataBuffer><DaqHost>localhost</DaqHost><DaqPort>8992</DaqPort></DataBuffer></config>";
 
   char answer[1000];
   socksrv->send(config_contents.c_str(),config_contents.length());
   socksrv->recv(answer,1000);
-  Log(info,"Got answer [%s]",answer);
+  cout << "Got answer [" << answer << "]" << endl;
 
   g_is_configured = true;
   //return 0;
 }
-
+/*
 void control_thread() {
   std::thread::id thread_id = std::this_thread::get_id();
   std::ostringstream stream;
@@ -247,64 +260,115 @@ void* reader_thread(void *arg) {
   }
   return NULL;
 }
+*/
 
 void receive_data() {
-  Log(info,"Working in reader_thread!!!");
+  // Important to avoid mangling of the output due to race conditions to the
+  // IO buffers. The trick is to use unbuffered (ie. direct) output
+  std::cout.setf(std::ios::unitbuf);
+
+  cout << "Working in reader_thread!!!" << endl;
 
   std::thread::id thread_id = std::this_thread::get_id();
   std::ostringstream stream;
   stream << std::hex << thread_id << " " << std::dec << thread_id;
-  Log(info,"#### Reader thread: %s",stream.str().c_str());
+  cout << "#### Receiving thread: %s",stream.str() << endl;
 
-  // Create a client that connects to the PTB
+  // Create a server that is meant to receive the data from the
+  // PTB and keep dumping the contents into somewhere...like a binary file
   try{
 
     // Create a tcp server and listen to the connection
     TCPServerSocket servSock(8992);
     // Accept a client
-    Log(info,"Entering waiting state");
+    cout << "Opening the receiver socket" << endl;
     TCPSocket *sock = servSock.accept();
-    Log(info,"--> Received a client request.");
+    cout << "--> Received a client request." << endl;
+
+    // -- Use the content header to parse the received data
+    // Also, might want to expand the rollover timestamps into full timestamps
+
+    // -- open an output file.
+    // -- This is an example, so just dump into some random format
+    // let's say, one word/frame per line in binary format in a file
+    // the frames will have the full timestamp already expanded
+
+    // -- the idea here is to show how the parsing would normally work
+
+    ptb::content::word::word_type wt;
+
+
 
     uint16_t pckt_size = 0;
     uint16_t bytes_collected= 0;
-    uint32_t header;
-    uint8_t body[4096];
-
+    ptb::content::tcp_header header;
+    ptb::content::word::header *hdr;
+    //uint32_t header;
+    uint8_t tcp_body[4096];
+    size_t count = 0;
     bool timeout_reached = false;
     // FIXME: Receive the ethernet packets
     while ((!g_stop_requested) || (g_stop_requested && !timeout_reached)) {
       // grab a header
-      sock->recv(&header,sizeof(header));
+      sock->recv(&header,header.word.size_bytes);
+      count++;
+      if (!(count%1000)) cout << "Counting " << count << "packets received..." << endl;
+      // check the number of bytes in this packet
+      while ()
+      bytes_collected = 0;
       // Cast the result into the header
-      ptb::content::tcp_header_t *pkg_hdr = reinterpret_cast<ptb::content::tcp_header_t*>(&header);
-      pckt_size = pkg_hdr->packet_size;
-      while (bytes_collected != pckt_size) {
-        sock->recv(&body[bytes_collected],(int)(pckt_size-bytes_collected));
+//      ptb::content::tcp_header_t *pkg_hdr = reinterpret_cast<ptb::content::tcp_header_t*>(&header);
+//      pckt_size = pkg_hdr->packet_size;
+      while (bytes_collected != header.word.packet_size) {
+        sock->recv(&tcp_body[bytes_collected],(int)(pckt_size-bytes_collected));
       }
+      // -- We now should have the whole sent packet
+      // parse it
+      uint32_t pos = 0;
+      while (pos <= header.word.packet_size) {
+        // 1. Grab the header (we know what it its size)
+        hdr = reinterpret_cast<ptb::content::word::header *>(&tcp_body[pos]);
+        pos += hdr->word.size_bytes;
+
+        // -- For now we are assuming that all payloads are of the same size
+        switch(hdr->word.word_type) {
+        case ptb::content::word::t_warn:
+          cout << "Received a warning!!! This is rare! Do something smart to fix the problem" << endl;
+          // advance the pointer by the size of this payload
+          pos += ptb::content::word::body_t::size_bytes;
+          break;
+        case ptb::content::word::t_gt:
+          cout << "Received a global trigger. Do something here on how to parse it" << endl;
+          // advance the pointer by the size of this payload
+          pos += ptb::content::word::body_t::size_bytes;
+          break;
+        case ptb::content::word::t_lt:
+          cout << "Received a low level trigger. Do something here on how to parse it" << endl;
+          // advance the pointer by the size of this payload
+          pos += ptb::content::word::body_t::size_bytes;
+          break;
+        case ptb::content::word::t_ts:
+          // Received a timestamp. Print it...why the hell not?
+          ptb::content::word::payload::timestamp_t *ts = reinterpret_cast<ptb::content::word::payload::timestamp_t *>(&tcp_body[pos]);
+          cout << "Received timestamp " << ts->timestamp << endl;
+          pos += ptb::content::word::body_t::size_bytes;
+          break;
+        }
+      }
+
     }
   }
   catch(...) {
-    ;
+    cerr << "Something went wrong. Unsure what" << endl;
   }
 }
-// void* shutdown(void *arg) {
-
-//   for (int i = 0; i < 3; ++i) {
-//     std::this_thread::sleep_for (std::chrono::seconds(1));
-//     Log(info) << i+1 << " seconds elapsed." << endlog;
-//   }
-
-//   ConfigServer *cfg = (ConfigServer*)arg;
-//   cfg->Shutdown();
-//   return NULL;
-// }
 
 int main() {
   enum commands {init=1,start=2,stop=3};
-  Logger::SetSeverity(Logger::debug);
   // Set the stop condition to false to wait for the data
+  g_is_running = false;
   g_stop_requested = false;
+  g_is_configured = false;
   std::signal(SIGINT, signalHandler);
 
 // This doesn't work since the thread gets stuck in the constructor of ConfigServer
@@ -316,13 +380,13 @@ int main() {
 std::thread::id thread_id = std::this_thread::get_id();
 std::ostringstream stream;
 stream << std::hex << thread_id << " " << std::dec << thread_id;
-Log(info,"#### Main thread: %s",stream.str().c_str());
+cout << "#### Main thread: " << stream.str() << endl;
 // Create a thread that passes the configuration
 
 int command = -1;
 int result = 0;
 try{
-  Log(info,"### Establishing control connection to the PTB");
+  cout << "### Establishing control connection to the PTB" << endl;
   TCPSocket socksrv("localhost", 8991);
   std::this_thread::sleep_for(std::chrono::seconds(3));
 
@@ -334,6 +398,8 @@ try{
     cout << "### Select command : " << endl;
     cout << " 1 : Init" << endl;
     cout << " 2 : Start Run" << endl;
+    cout << "\n\n After starting a run, you can stop the run with Ctrl+C\n\n" << endl;
+
 //    cout << " 3 : Stop Run" << endl;
 //    cout << " 4 : Soft Reset" << endl;
 //    cout << " 5 : Hard Reset" << endl;
@@ -360,7 +426,7 @@ try{
         break;
     }
   }
-  Log(warning,"### Stop requested.");
+  cout << "### Stop requested." << endl;
   // Wait for the reading function to return
   reader.join();
 
